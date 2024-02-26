84fb30fade8883c92bf5afa8095f105e03bd17c3
kvazaras <martas.ambraziunas@gmail.com>
2024-02-26 17:21:15 +0000

    Revert "Added change text functionality"
    This reverts commit b696166173c9d55c64593201de7b78b128e37dca.

--- .idea/sonarlint/securityhotspotstore/index.pb
+++ .idea/sonarlint/securityhotspotstore/index.pb
@@ -5,9 +5,3 @@ P
 build.gradle.kts,d\b\dbcff70658daf80b53ce624f6adcaa529df5ed8d
 D
 app/build.gradle.kts,d\2\d2a4383452aad84a18429a5844d31330a012a9e6
-p
-@app/src/main/java/com/example/fthelloworld2024/MainActivity.java,8\a\8aa0a05bab6cc2299d3fea1ad894959628943758
-S
-#.idea/sonarlint/issuestore/index.pb,9\f\9fe84ebb15faf917b7def6236dba604453cc61e0
-]
--.idea/sonarlint/securityhotspotstore/index.pb,8\8\88a9255124c95bdc913197c120a8d560edc59c8e
--- app/src/main/java/com/example/fthelloworld2024/MainActivity.java
+++ app/src/main/java/com/example/fthelloworld2024/MainActivity.java
@@ -3,22 +3,12 @@ package com.example.fthelloworld2024;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
-import android.view.View;
-import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
-
-    private TextView tvMain;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        this.tvMain = findViewById(R.id.tvMain);
-    }
-
-    public void btnChangeTextClick(View view) {
-        this.tvMain.setText("Hello");
     }
 }
